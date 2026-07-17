694f47d70d140a5c51e965cddbd3eb16fe4b3c9b
Ubuntu <ubuntu@ip-172-31-7-88.ap-south-1.compute.internal>
2026-07-17 08:48:07 +0000

    Initial app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Jenkins + Docker CI/CD pipeline!');
+});
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
